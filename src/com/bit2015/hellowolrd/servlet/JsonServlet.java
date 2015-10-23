0c4592418b5f8378353ecdb4946df8c36300ef5b
bit-user <bit-user@bit>
2015-10-23 20:54:44 +0900

    1

---
+++ src/com/bit2015/hellowolrd/servlet/JsonServlet.java
@@ -0,0 +1,43 @@
+package com.bit2015.hellowolrd.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class JsonServlet
+ */
+@WebServlet("/json")
+public class JsonServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doPost(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.setContentType("application/json;charset=utf-8");
+
+		PrintWriter out = response.getWriter();
+		out.print("{ \"name\": \"test\", \"message\": \"hello\" }");
+
+	}
+
+}
